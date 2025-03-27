c676eaf4d66cec9ab5d42aaed959cb1599e7a1b7
Kaige Cai <caikaige.g@gmail.com>
2025-03-27 16:03:47 +0800

    .

--- src/main/kotlin/RandomNumber.kt
+++ src/main/kotlin/RandomNumber.kt
@@ -1,15 +1,13 @@
 import kotlin.random.Random
 
 fun generateUniqueRandomNumbersShuffled(): List<Int> {
-    // 创建1~500的数字列表
+    // 创建1~508的数字列表
     val numbers = (1..508).toMutableList()
-
     // 洗牌前5个数字
     for (i in 0 until 5) {
         val j = Random.nextInt(i, numbers.size)
         numbers[i] = numbers[j].also { numbers[j] = numbers[i] }
     }
-
     // 取前5个
     return numbers.take(5)
 }
@@ -17,12 +15,10 @@ fun generateUniqueRandomNumbersShuffled(): List<Int> {
 fun generateUniqueRandomNumbers(): List<Int> {
     // 使用 HashSet 来存储不重复的数字
     val numbers = HashSet<Int>()
-
     // 当集合大小小于5时，继续生成随机数
     while (numbers.size < 5) {
-        numbers.add(Random.nextInt(1, 509)) // 生成1~500的随机数
+        numbers.add(Random.nextInt(1, 509)) // 生成1~508的随机数
     }
-
     // 转换为List返回
     return numbers.toList()
 }
